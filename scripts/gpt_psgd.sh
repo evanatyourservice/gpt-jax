7795ce58cf97979991b55b0df86a2cee8be2678c
Evan Walters <evanatyourservice@gmail.com>
2024-08-25 15:31:53 -0600

    Update gpt_psgd.sh

--- scripts/gpt_psgd.sh
+++ scripts/gpt_psgd.sh
@@ -13,6 +13,6 @@ python3 train.py \
     --optimizer.weight_decay=0.01 \
     --optimizer.preconditioner_update_probability=0.5 \
     --optimizer.max_size_triangular=1000000000 \
-    --optimizer.max_skew_triangular=10 \
+    --optimizer.max_skew_triangular=0 \
     --optimizer.precond_lr=0.1 \
     --optimizer.precond_init_scale=1.0
