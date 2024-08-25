01112df8dba74569fd0a927ef992e75135f95ed3
Evan Walters <evanatyourservice@gmail.com>
2024-08-25 16:09:07 -0600

    psgd in float32

--- scripts/gpt_psgd.sh
+++ scripts/gpt_psgd.sh
@@ -13,6 +13,6 @@ python3 train.py \
     --optimizer.grad_clip=0.0 \
     --optimizer.preconditioner_update_probability=0.5 \
     --optimizer.max_size_triangular=1000000000 \
-    --optimizer.max_skew_triangular=0 \
+    --optimizer.max_skew_triangular=10 \
     --optimizer.precond_lr=0.1 \
     --optimizer.precond_init_scale=1.0
