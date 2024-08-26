ead00d7dfa66805e596d147e42893b66b3e81975
Evan Walters <evanatyourservice@gmail.com>
2024-08-25 18:50:05 -0600

    settings

--- scripts/gpt_psgd.sh
+++ scripts/gpt_psgd.sh
@@ -13,7 +13,7 @@ python3 train.py \
     --optimizer.type=affine \
     --optimizer.weight_decay=0.01 \
     --optimizer.preconditioner_update_probability=0.2 \
-    --optimizer.max_size_triangular=1000000000 \
+    --optimizer.max_size_triangular=0 \
     --optimizer.max_skew_triangular=0 \
     --optimizer.precond_lr=0.1 \
     --optimizer.precond_init_scale=1.0 \
